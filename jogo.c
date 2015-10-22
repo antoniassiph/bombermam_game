0a77da9479a0b7ce57b02178de7e54acdb577142
antoniassiph <paulos@alunos.utfpr.edu.br>
2015-10-22 11:21:37 -0200

    Removido include do C++

--- jogo.c
+++ jogo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <iostream>
 #include <stdio.h>
 #include "headers/getch_getche.h" //funcao getche e getch do windows
 #include <string.h>
