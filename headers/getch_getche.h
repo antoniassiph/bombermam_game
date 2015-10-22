c82a06ec52d7b01f778a54f08ef4492281c7db75
Paulo Antoniassi <antoniassiph@gmail.com>
2015-10-22 10:53:59 -0200

    Corrigido nomes dos includes e funcção kbhit colocada como include

--- headers/getch_getche.h
+++ headers/getch_getche.h
@@ -1,9 +1,11 @@
 // This is start of the header guard.  ADD_H can be any unique name.  By convention, we use the name of the header file.
-#ifndef ADD_H
-#define ADD_H
+#ifndef GETCH_H
+#define GETCH_H
 #include <termios.h>
 #include <stdio.h>
 // This is the content of the .h file, which is where the declarations go
+
+
 static struct termios old, newt;
 
 /* Initialize new terminal i/o settings */
